75a500c46268811828527991c7b574a5f4543ddf
Kirk Kenney <kirk_kenney_1@hotmail.co.uk>
2020-09-30 18:47:56 +0100

    added styling for larger screen sizes

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInfo } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Header = props => {
     return (
         <div className='header'>
-            <div>
-                <FontAwesomeIcon icon={faInfo} />
-            </div>
             <div>
                 <FontAwesomeIcon icon={faUser} onClick={props.onUserSchemesClick} />
             </div>
